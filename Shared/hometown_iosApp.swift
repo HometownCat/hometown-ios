ddb82e388734dec7ffa4a9179338351bbff3ce01
uiurihappy <uiurihappy@sju.ac.kr>
2022-08-18 16:31:12 +0900

    feat: First Commit

---
+++ Shared/hometown_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hometown_iosApp.swift
+//  Shared
+//
+//  Created by 차윤범 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct hometown_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
