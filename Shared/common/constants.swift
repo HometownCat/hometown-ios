d0079afcfd5e0a6be53136904e7fbe69f0f71c0c
Juwon-Yun <eooll2@daum.net>
2022-08-28 15:13:37 +0900

    modify: dir 구조

---
+++ Shared/common/constants.swift
@@ -0,0 +1,8 @@
+//
+//  constants.swift
+//  hometown_ios (iOS)
+//
+//  Created by 윤주원 on 2022/08/28.
+//
+
+import Foundation
---
+++ Shared/view_model/view_model.swift
@@ -0,0 +1,8 @@
+//
+//  viewModel.swift
+//  hometown_ios (iOS)
+//
+//  Created by 윤주원 on 2022/08/28.
+//
+
+import Foundation
