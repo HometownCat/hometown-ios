4949f0c279f3ce91a30b2a180de6ab3cd1814016
Juwon-Yun <eooll2@daum.net>
2022-09-18 10:51:32 +0900

    refactor: customTextField, customSecureField.swift

---
+++ Shared/View/Components/MainFeed.swift
@@ -0,0 +1,20 @@
+//
+//  MainFeed.swift
+//  hometown_ios (iOS)
+//
+//  Created by 윤주원 on 2022/09/18.
+//
+
+import SwiftUI
+
+struct MainFeed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainFeed_Previews: PreviewProvider {
+    static var previews: some View {
+        MainFeed()
+    }
+}
